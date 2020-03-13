e802ac6aa1e29ea26995e0511d1352df65898bb5
tiagoribeirocb <tiago.ribeiro@codebuddy.com.br>
2020-03-13 13:44:56 -0300

    ...

--- stock/models/AbstractModel.py
+++ stock/models/AbstractModel.py
@@ -1,6 +1,9 @@
 import locale
 
 class AbstractModel():
+    def __init__(self):
+        locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
     def format_money(self, value):
         valor = value
         locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
